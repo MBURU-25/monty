4b96166058484d908bee4ec9529ab04dabfe9ef1
MBURU-25 <njerimburu07@gmail.com>
2023-01-17 03:35:57 -0800

    main.c

--- main.c
+++ main.c
@@ -10,7 +10,6 @@ int main(int argc, char *argv[])
 {
 	char *content;
 	FILE *file;
-	size_t size = 0;
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
@@ -30,7 +29,6 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
